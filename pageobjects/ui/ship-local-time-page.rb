8fa631a8e7137c63cd87ca1ecd9e7a12057c9a11
gengxian <gengxian.chua@sol-x.co>
2020-05-21 11:15:45 +0800

    - fix 24hr time bug

--- pageobjects/ui/ship-local-time-page.rb
+++ pageobjects/ui/ship-local-time-page.rb
@@ -2,7 +2,7 @@ require './././support/env'
 
 class ShipLocalTimePage
   include PageObject
-
+  
   button(:clock_btn,xpath: "//button[starts-with(@class,'Clock__ClockButton')]")
   element(:clock,xpath: "//button[starts-with(@class,'Clock__ClockButton')]/h3")
   span(:utc_time,xpath: "//span[@data-testid='utc-time']")
@@ -11,28 +11,38 @@ class ShipLocalTimePage
   button(:decrement,xpath: "//div[starts-with(@class,'ClockModal__')]/button[1]")
   button(:increment,xpath: "//div[starts-with(@class,'ClockModal__')]/button[2]")
   
-
+  
   def is_utc_time
     # clock_btn
     # sleep 1
     return Time.now.utc.strftime("%I:%M") if Time.now.utc.strftime("%k") < "12"
     return "#{Time.now.utc.strftime("%k:%M")}" if Time.now.utc.strftime("%k") > "12"
   end
-
+  
   def adjust_ship_local_time
     clock_btn
     ["1","2"].sample === "1" ? decrement : increment
   end
-
+  
   def is_update_ship_time
     sleep 1
     clock_btn
     sleep 1
-    tmp = ServiceUtil.get_response_body['data']['currentTime']['utcOffset']
+    get_current_offset = ServiceUtil.get_response_body['data']['currentTime']['utcOffset']
     current_time = utc_time.split(':')
-    current_time[0].to_i < 10 ? new_hour = "0#{current_time[0].to_i+tmp}" : new_hour = "#{current_time[0].to_i+tmp}"
+    new_hour = cal_new_offset_time(get_current_offset,current_time)
     Log.instance.info(">>> #{new_hour}:#{current_time[1]}")
-    return ((utc_time_text === "Local Time #{tmp}h") && (utc_timezone_elements[1].text === "#{new_hour}:#{current_time[1]}")) if tmp < 0
-    return ((utc_time_text === "Local Time +#{tmp}h") && (utc_timezone_elements[1].text === "#{new_hour}:#{current_time[1]}")) if tmp >= 0
+    return ((utc_time_text === get_new_current_offset_text(get_current_offset)) && (utc_timezone_elements[1].text === "#{new_hour}:#{current_time[1]}"))
   end
+  
+  private
+  
+  def get_new_current_offset_text(get_current_offset)
+    get_current_offset < 0 ? "Local Time #{get_current_offset}h" : "Local Time +#{get_current_offset}h"
+  end
+
+  def cal_new_offset_time(get_current_offset,current_time)
+    current_time[0].to_i < 10 ? "0#{current_time[0].to_i+get_current_offset}" : "#{current_time[0].to_i+get_current_offset}"
+  end
+
 end
