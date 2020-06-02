383aaf88a3e748518ffc1e27c08376fe9aeac173
gengxian <gengxian.chua@sol-x.co>
2020-06-02 15:25:05 +0800

    - added crew detail base - change jira ticket prefix

--- library/extent/report-utils.rb
+++ library/extent/report-utils.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class ReportUtils
   def self.get_screenshot(browser)
-    fullpath = "#{Dir.getwd}/testreport/reports/screenshots/screenshot#{Time.now.strftime("%Y_%m_%d-%HH_%MM_%SS")}.png"
-    screenshotpath = fullpath.gsub("#{Dir.getwd}/testreport/reports", ".")
+    fullpath = "#{Dir.getwd}/testreport/reports/screenshots/screenshot#{Time.now.strftime('%Y_%m_%d-%HH_%MM_%SS')}.png"
+    screenshotpath = fullpath.gsub("#{Dir.getwd}/testreport/reports", '.')
     sleep 1
     browser.save_screenshot fullpath
     screenshotpath
@@ -11,12 +13,12 @@ class ReportUtils
 
   def self.insert_example_values_into_step(step, examples)
     step_holders = step.scan(/<\w*\S*>/).to_a
-    if step_holders.size != 0
-      for i in 0..step_holders.size - 1
-        step = step.gsub(step_holders[i].to_s, examples[step_holders[i].gsub("<", "").gsub(">", "")].to_s)
+    unless step_holders.empty?
+      (0..step_holders.size - 1).each do |i|
+        step = step.gsub(step_holders[i].to_s, examples[step_holders[i].gsub('<', '').gsub('>', '')].to_s)
       end
     end
-    return step
+    step
   end
 
   private
@@ -31,27 +33,29 @@ class ReportUtils
 
   def self.get_steps(feature_, scenario)
     arr_steps = []
-    if feature_.class.to_s == "Cucumber::RunningTestCase::ScenarioOutlineExample"
+    if feature_.class.to_s == 'Cucumber::RunningTestCase::ScenarioOutlineExample'
       # match scenario
       feature_.feature.feature_elements.size.times do |z|
-        if feature_.feature.feature_elements[z].to_s == scenario.name.gsub(/, Examples \(#\d*\)/, "")
-          @outline_examples = feature_.feature.feature_elements[z].examples_tables[0].example_rows.to_s.scan(/{([^}]*)/)
-          for i in 1..feature_.scenario_outline.step_count.to_i
-            step = feature_.feature.feature_elements[z].steps[i - 1]
-            example_values = eval("{+#{@outline_examples[$examples_count][0]}}")
-            arr_steps << insert_example_values_into_step(step.to_s, example_values)
-          end
-          break
+        unless feature_.feature.feature_elements[z].to_s == scenario.name.gsub(/, Examples \(#\d*\)/, '')
+          next
         end
+
+        @outline_examples = feature_.feature.feature_elements[z].examples_tables[0].example_rows.to_s.scan(/{([^}]*)/)
+        (1..feature_.scenario_outline.step_count.to_i).each do |i|
+          step = feature_.feature.feature_elements[z].steps[i - 1]
+          example_values = eval("{+#{@outline_examples[$examples_count][0]}}")
+          arr_steps << insert_example_values_into_step(step.to_s, example_values)
+        end
+        break
       end
       reset_examples_counter(@outline_examples)
     else
       # match scenario
       feature_.feature_elements.size.times do |z|
-        if feature_.feature_elements[z].to_s == scenario.name.to_s
-          feature_.feature_elements[z].children.each do |step|
-            arr_steps << step
-          end
+        next unless feature_.feature_elements[z].to_s == scenario.name.to_s
+
+        feature_.feature_elements[z].children.each do |step|
+          arr_steps << step
         end
       end
     end
@@ -60,26 +64,32 @@ class ReportUtils
 
   def self.output_tag(scenario, extent_test)
     scenario.source_tag_names.each do |tag|
-      if tag.include? "SLO-"
-        extent_test.assign_category($obj_env_yml["jira_url"] + tag.to_s.tr('@"[]]', ""))
+      if tag.include? 'SOL-'
+        extent_test.assign_category($obj_env_yml['jira_url'] + tag.to_s.tr('@"[]]', ''))
       else
-        extent_test.assign_category(tag.to_s.tr('@"[]]', ""))
+        extent_test.assign_category(tag.to_s.tr('@"[]]', ''))
       end
     end
   end
 
   def self.make_folder(folder_name)
     Dir.mkdir("testreport/#{folder_name}")
-    Dir["testreport/*"].each do |subfolder|
-      FileUtils.cp_r(subfolder, "testreport/#{folder_name}/") unless subfolder.to_s.include? "@"
-      FileUtils.cp_r(subfolder, "testreport/#{folder_name}/") if ["jsonreports", "log", "reports", "xmlreports"].include? subfolder.to_s
+    Dir['testreport/*'].each do |subfolder|
+      unless subfolder.to_s.include? '@'
+        FileUtils.cp_r(subfolder, "testreport/#{folder_name}/")
+      end
+      if %w[jsonreports log reports xmlreports].include? subfolder.to_s
+        FileUtils.cp_r(subfolder, "testreport/#{folder_name}/")
+      end
     end
   end
 
   def self.clear_folder
-    Dir["testreport/*"].each do |subfolder|
-      FileUtils.rm_r(subfolder) if ["jsonreports", "log", "reports", "xmlreports"].include? subfolder.to_s
-      FileUtils.rm_r(subfolder) unless subfolder.to_s.include? "@"
+    Dir['testreport/*'].each do |subfolder|
+      if %w[jsonreports log reports xmlreports].include? subfolder.to_s
+        FileUtils.rm_r(subfolder)
+      end
+      FileUtils.rm_r(subfolder) unless subfolder.to_s.include? '@'
     end
   end
 
@@ -100,5 +110,4 @@ class ReportUtils
   #   # end
   #   # @overall_steps
   # end
-
 end
