3aadf966397522462f05adb12c5bf868ba0b0775
gengxian <gengxian.chua@sol-x.co>
2020-06-30 09:33:03 +0800

    - reverting back to root url - increase wait time

--- stepdefinitions/ui/common-steps.rb
+++ stepdefinitions/ui/common-steps.rb
@@ -4,7 +4,7 @@ Given (/^I launch sol-x portal$/) do
   step 'I unlink all crew from wearable'
   sleep 1
   $browser.get(EnvironmentSelector.get_environment_url)
-  sleep 2
+  sleep 4
   # $browser.navigate.refresh
   # sleep 1
   $browser.navigate.refresh
