211d5f89670885d779ee2da80cecca609f0e5400
gx-solx <63989831+gx-solx@users.noreply.github.com>
2020-07-09 20:08:52 +0800

    update sleep

--- stepdefinitions/ui/common-steps.rb
+++ stepdefinitions/ui/common-steps.rb
@@ -4,7 +4,7 @@ Given (/^I launch sol-x portal$/) do
   step 'I unlink all crew from wearable'
   sleep 1
   $browser.get(EnvironmentSelector.get_environment_url)
-  sleep 2
+  sleep 3
   # puts "screen size: #{$browser.window_size}"
 end
 
