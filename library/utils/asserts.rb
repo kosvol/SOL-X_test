3c57bcb3a7bcda6e7eecd5eec2fcf20a49c68734
gengxian <gengxian.chua@sol-x.co>
2020-08-07 13:42:27 +0800

    - update exists method

--- library/utils/asserts.rb
+++ library/utils/asserts.rb
@@ -49,10 +49,12 @@ module AssertionUtil
 
   def not_to_exists(element)
     expect(element).not_to exist
+  rescue StandardError
   end
 
   def to_exists(element)
     expect(element).to exist
+  rescue StandardError
   end
 
   # def is_not_visible(element)
