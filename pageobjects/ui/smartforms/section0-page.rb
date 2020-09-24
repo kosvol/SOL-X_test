42f948b91eb4626e5504690beb23dd08e23bb68c
gengxian <gengxian.chua@sol-x.co>
2020-09-24 14:27:36 +0800

    - remove extra back element

--- pageobjects/ui/smartforms/section0-page.rb
+++ pageobjects/ui/smartforms/section0-page.rb
@@ -9,7 +9,6 @@ class Section0Page < CommonFormsPage
   elements(:created_ptw_id, xpath: '//li[1]/span')
   element(:ptw_id, xpath: "//nav[starts-with(@class,'NavigationBar__NavBar-')]/header/h3")
   button(:click_permit_type_ddl, xpath: "//button[@id='permitType']")
-  # button(:back_btn, xpath: "//div[@class='action']/button[starts-with(@class,'Button__ButtonStyled')]")
   button(:save_btn, xpath: "//div[starts-with(@class,'Section__Description')]/div/button[starts-with(@class,'Button__ButtonStyled')]")
   buttons(:list_permit_type, xpath: '//ul/li/button')
   # pending approval permit
