c4e0c4a8e735efff226a97da1486e4e313db75c4
gengxian <gengxian.chua@sol-x.co>
2020-10-26 09:39:24 +0800

    - print url

--- library/helpers/environment-selector.rb
+++ library/helpers/environment-selector.rb
@@ -1,21 +1,24 @@
+# frozen_string_literal: true
+
 module EnvironmentSelector
   class << self
     def get_environment_url
-      $obj_env_yml["#{get_environment}"]["#{get_application}"]
+      p "URL: #{$obj_env_yml[get_environment.to_s][get_application.to_s]}"
+      $obj_env_yml[get_environment.to_s][get_application.to_s]
     end
 
     def get_graphql_environment_url(key)
-      $obj_env_yml["#{get_environment}"]["#{key}"]
+      $obj_env_yml[get_environment.to_s][key.to_s]
     end
 
     private
 
     def get_environment
-      $current_platform = ENV["ENVIRONMENT"]
+      $current_platform = ENV['ENVIRONMENT']
     end
 
     def get_application
-      $current_platform = ENV["APPLICATION"]
+      $current_platform = ENV['APPLICATION']
     end
   end
 end
