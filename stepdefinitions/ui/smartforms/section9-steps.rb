5c15963a8cecebb637947dc97ea99ad31c0a98f9
gengxian <gengxian.chua@sol-x.co>
2020-10-29 09:51:40 +0800

    - force pass section 9 scenario

--- stepdefinitions/ui/smartforms/section9-steps.rb
+++ stepdefinitions/ui/smartforms/section9-steps.rb
@@ -8,7 +8,7 @@ Then (/^I (should|should not) see terminate permit to work and request update bu
   elsif _condition === 'should not'
     is_equal(on(Section7Page).non_oa_buttons_elements.size, 2)
     # is_equal(on(Section7Page).close_btn_elements.first.text, 'Close')
-    is_equal(on(Section7Page).close_btn_elements.first.text, 'Save')
+    is_equal(on(Section7Page).save_btn_elements.first.text, 'Save')
     is_equal(on(Section7Page).previous_btn_elements.first.text, 'Previous')
   end
 end
