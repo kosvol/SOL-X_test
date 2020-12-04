3596bae573d6da27dd4a91b872cba43aa216c973
gengxian <gengxian.chua@sol-x.co>
2020-12-04 09:14:31 +0800

    - rebase script

--- stepdefinitions/ui/smartforms/section1-steps.rb
+++ stepdefinitions/ui/smartforms/section1-steps.rb
@@ -21,7 +21,7 @@ Then (/^I should see a list of (sea states|wind forces)$/) do |_state, _table|
 end
 
 Then (/^I should not see previous button exists$/) do
-  is_equal(on(Section1Page).btn_list_elements[0].text, 'Save & Next')
+  is_equal(on(Section1Page).btn_list_elements[0].text, 'Next')
 end
 
 Then (/^I (should|should not) see maintenance duration section and require text$/) do |condition|
