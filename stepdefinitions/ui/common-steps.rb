9071371ef14e4bd620d65ac891d3a2afd8c76402
gengxian <gengxian.chua@sol-x.co>
2020-12-08 14:44:04 +0800

    - added delay for first load

--- stepdefinitions/ui/common-steps.rb
+++ stepdefinitions/ui/common-steps.rb
@@ -35,7 +35,7 @@ end
 
 Given (/^I launch sol-x portal without unlinking wearable$/) do
   $browser.get(EnvironmentSelector.get_environment_url)
-  sleep 5
+  sleep 6
   # puts "screen size: #{$browser.window_size}"
 end
 
