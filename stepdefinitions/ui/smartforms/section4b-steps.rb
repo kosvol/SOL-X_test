067274f8afda7307fad3bba996a2b354a57ed596
gengxian <gengxian.chua@sol-x.co>
2020-12-08 14:58:16 +0800

    - refactor step

--- stepdefinitions/ui/smartforms/section4b-steps.rb
+++ stepdefinitions/ui/smartforms/section4b-steps.rb
@@ -29,7 +29,8 @@ When (/^I select yes to EIC$/) do
 end
 
 When (/^I select yes to EIC certification$/) do
-  on(Section4BPage).yes_no_btn_elements[0].click
+  step 'I select yes to EIC'
+  # on(Section4BPage).yes_no_btn_elements[0].click
   step 'I set time'
   on(Section4BPage).create_eic_btn
   sleep 2
