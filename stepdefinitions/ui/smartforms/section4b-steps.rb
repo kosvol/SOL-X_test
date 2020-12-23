c2bd402984697f07d51ed6bf3891e77a8ab8fbce
gengxian <gengxian.chua@sol-x.co>
2020-12-23 17:48:06 +0800

    - debug offline mode

--- stepdefinitions/ui/smartforms/section4b-steps.rb
+++ stepdefinitions/ui/smartforms/section4b-steps.rb
@@ -25,7 +25,6 @@ end
 
 Then (/^I should see wifi restore popup display for (.*)$/) do |which_category|
   step 'I turn on wifi'
-  sleep 1
   if which_category === "EIC"
     is_equal(on(Section4BPage).wifi_popup_elements[1].text,"Wi-Fi restored")
     is_equal(on(Section4BPage).wifi_popup_elements[2].text,"You are Online Now\nNow you can submit Energy Isolation Certification so other crew members will be able to access it in other devices.")
