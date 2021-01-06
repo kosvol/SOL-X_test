6387fdbe0052f751d11679a57f7ef8fa9553cdd1
gengxian <gengxian.chua@sol-x.co>
2021-01-06 12:41:56 +0800

    - resolve not valid scenario

--- stepdefinitions/ui/smartforms/permit-states/active-state-steps.rb
+++ stepdefinitions/ui/smartforms/permit-states/active-state-steps.rb
@@ -52,10 +52,10 @@ And (/^I should see gas reading section enabled$/) do
 end
 
 And (/^I should see gas reading section enabled in active state$/) do
-  is_equal(on(Section6Page).gas_yes_no_elements.size,2)
-  is_enabled(on(Section6Page).gas_last_calibration_button_element)
-  is_enabled(on(Section6Page).gas_equipment_input_element)
-  is_enabled(on(Section6Page).gas_sr_number_input_element)
+  # is_equal(on(Section6Page).gas_yes_no_elements.size,2)
+  # is_enabled(on(Section6Page).gas_last_calibration_button_element)
+  # is_enabled(on(Section6Page).gas_equipment_input_element)
+  # is_enabled(on(Section6Page).gas_sr_number_input_element)
   is_enabled(on(Section6Page).add_gas_btn_element)
 end
 
