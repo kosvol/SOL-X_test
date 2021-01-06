0e6dc2f3618f823777a82e75c87c4c65b5b26528
gengxian <gengxian.chua@sol-x.co>
2021-01-06 09:46:54 +0800

    - stabilize core changes

--- stepdefinitions/ui/common/visibility-steps.rb
+++ stepdefinitions/ui/common/visibility-steps.rb
@@ -25,6 +25,10 @@ Then (/^I should see (.*) button (disabled|enabled)$/) do |_which_button,_condit
     case _which_button
     when "sign"
       is_enabled(on(Section5Page).sign_btn_role_elements.first)
+    when "submit"
+      step 'I dismiss gas reader dialog box'
+      on(Section3APage).scroll_multiple_times(3)
+      is_enabled(on(CommonFormsPage).submit_for_master_approval_btn_elements.first)
     end
   end
 end
