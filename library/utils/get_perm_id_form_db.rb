d526799f9be29fc543bf602005d7edd354760ff1
gengxian <gengxian.chua@sol-x.co>
2021-01-27 23:40:20 +0800

    - added delay; for chrome cache entry; crashing chrome

--- library/utils/get_perm_id_form_db.rb
+++ library/utils/get_perm_id_form_db.rb
@@ -4,7 +4,7 @@ module WorkWithIndexeddb
     def get_id_from_indexeddb(temp_id)
       # works only after synchronization. Usually you should return to the Home page.
       # otherwise the browser shut down
-      sleep 4
+      sleep 6
       $browser.execute_script("openRequest = indexedDB.open('safevue')")
       sleep 2
       $browser.execute_script("db = openRequest.result")
