c761faf5f053c35b91f71838207c50d032de5cdb
gengxian <gengxian.chua@sol-x.co>
2021-01-28 22:36:22 +0800

    - added platform check for wifi toggle - removed scroll

--- support/browserutils/browser_setup.rb
+++ support/browserutils/browser_setup.rb
@@ -23,7 +23,7 @@ class BrowserSetup
       # $browser.manage.timeouts.page_load = 30
       $browser.manage.timeouts.implicit_wait = 30
     end
-    BrowserActions.turn_on_wifi_by_default
+    BrowserActions.turn_on_wifi_by_default if ENV['PLATFORM'].upcase != "CHROME"
     $browser
   end
 
