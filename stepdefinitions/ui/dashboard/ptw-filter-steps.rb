732ac561a188d1d9bb7fd5ff0b8db916ee90382c
gengxian <gengxian.chua@sol-x.co>
2021-01-31 12:09:37 +0800

    - refactored permit filter

--- stepdefinitions/ui/dashboard/ptw-filter-steps.rb
+++ stepdefinitions/ui/dashboard/ptw-filter-steps.rb
@@ -8,31 +8,31 @@ end
 
 Then (/^I should see (.+) permits listing match counter$/) do |_which_filter|
   sleep 3
-  # on(Section3APage).scroll_multiple_times(25)
   step 'I get forms-filter/smart-form-filter request payload'
   step 'I hit graphql'
   on(PtwFilterPage).get_permits_counter
+
   case _which_filter
   when 'pending approval'
-    if @total_pending_approval.to_i != 0
+    if $total_pending_approval.to_i != 0
       is_true(on(PtwFilterPage).is_permit_listing_count?(_which_filter))
     else
       to_exists(on(CommonFormsPage).no_permits_found_element)
     end
   when 'update needed'
-    if @total_update_needed.to_i != 0
+    if $total_update_needed.to_i != 0
       is_true(on(PtwFilterPage).is_permit_listing_count?(_which_filter))
     else
       to_exists(on(CommonFormsPage).no_permits_found_element)
     end
   when 'active'
-    if @total_active.to_i != 0
+    if $total_active.to_i != 0
       is_true(on(PtwFilterPage).is_permit_listing_count?(_which_filter))
     else
       to_exists(on(CommonFormsPage).no_permits_found_element)
     end
   when 'pending withdrawal'
-    if @total_terminal.to_i != 0
+    if $total_terminal.to_i != 0
       is_true(on(PtwFilterPage).is_permit_listing_count?(_which_filter))
     else
       to_exists(on(CommonFormsPage).no_permits_found_element)
