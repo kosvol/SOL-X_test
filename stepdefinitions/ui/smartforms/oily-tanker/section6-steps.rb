fcc29edda13c08a6ffeecd718e2a936b7a2ce63f
gengxian <gengxian.chua@sol-x.co>
2021-02-18 16:03:08 +0800

    - rebased new released

--- stepdefinitions/ui/smartforms/oily-tanker/section6-steps.rb
+++ stepdefinitions/ui/smartforms/oily-tanker/section6-steps.rb
@@ -12,7 +12,8 @@ end
 And (/^I should see incomplete signature field warning message display$/) do
   on(Section3APage).scroll_multiple_times(5)
   is_equal(on(Section6Page).info_warning_boxes_elements[2].text,"This permit has required fields missing. To submit it for approval, please sign at the following sections")
-  is_equal(on(Section6Page).info_warning_boxes_elements[3].text,"Helicopter Operation")
+  # is_equal(on(Section6Page).info_warning_boxes_elements[3].text,"Helicopter Operation")
+  does_include(on(Section6Page).info_warning_boxes_elements[3].text,"Helicopter Operation")
 end
 
 Then (/^I should see master (approval|review) button only$/) do |_condition|
