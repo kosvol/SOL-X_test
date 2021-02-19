e4fee435d23dfb433bee20fc13413b5a86c1dbd3
gengxian <gengxian.chua@sol-x.co>
2021-02-19 10:20:49 +0800

    - rebased

--- stepdefinitions/ui/smartforms/oily-tanker/section3a-steps.rb
+++ stepdefinitions/ui/smartforms/oily-tanker/section3a-steps.rb
@@ -7,7 +7,7 @@ end
 
 Then (/^I should see fields disabled$/) do
   is_equal(on(Section3APage).total_p_elements.size,14)
-  is_disabled(on(Section3APage).delete_btn_elements.first)
+  not_to_exists(on(Section3APage).delete_btn_elements.first)
 end
 
 And (/^I should see correct risk evaluation (.+),(.+),(.+)$/) do |_risk, _risk1, _risk2|
