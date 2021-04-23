3d336a055618ecef6676a1c3d370f0ad942501fb
gengxian <gengxian.chua@sol-x.co>
2021-04-23 09:44:37 +0800

    - addded sleep for signature component

--- stepdefinitions/ui/smartforms/oily-tanker/section3d-steps.rb
+++ stepdefinitions/ui/smartforms/oily-tanker/section3d-steps.rb
@@ -11,6 +11,7 @@ end
 
 And (/^I sign (checklist|section|DRA section 3d) with (.*) as (valid|invalid) pin$/) do |_page,_pin,_condition|
   @@entered_pin = _pin
+  sleep 1
   on(CommonFormsPage).sign_btn_elements.first.click
   step "I enter pin #{_pin}" if _condition === "invalid"
   step "I sign on canvas with valid #{@@entered_pin} pin" if _condition === "valid"
