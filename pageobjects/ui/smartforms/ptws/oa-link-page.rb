689b902b931f9dea8516c93cdf1d7bfefcfc4463
Konstantin Sergeenko <ksergeenko@ipolecat.com>
2021-06-24 00:37:44 +0300

    Update set_from_to_details method to maintain 'from' time  >23

--- pageobjects/ui/smartforms/ptws/oa-link-page.rb
+++ pageobjects/ui/smartforms/ptws/oa-link-page.rb
@@ -31,7 +31,7 @@ class OAPage < Section9Page
   element(:warning_link_expired, xpath: "//div[contains(@class, 'WarningLinkExpired')]/section")
 
   ## Web Confirmation Page
-  element(:topbar_header_h3, xpath: "//nav[contains(@class,'NavigationBar')]//h3")
+  #element(:topbar_header_h3, xpath: "//nav[contains(@class,'NavigationBar')]//h3")
   element(:main_header, xpath: "//h2[contains(@class, 'Heading__H2')]")
   element(:main_description, xpath: "//section[contains(@class, 'Section__SectionMain')]")
   elements(:confirmation_question, xpath: "//ul/li")
@@ -118,13 +118,28 @@ class OAPage < Section9Page
     ### set from time
     date_time_from_elements[1].click
     starttime = Time.now.utc.strftime('%k').to_i + 1
-    hour_from_picker_elements[starttime].click
-    sleep 1
-    minute_from_picker_elements[1].click
-    dismiss_picker_element.click
-    sleep 1
-    BrowserActions.js_click("//textarea[contains(@placeholder,'Optional')]")
-    p " #{starttime}"
+    if starttime <= 23
+      hour_from_picker_elements[starttime].click
+      sleep 1
+      minute_from_picker_elements[1].click
+      dismiss_picker_element.click
+      sleep 1
+      BrowserActions.js_click("//textarea[contains(@placeholder,'Optional')]")
+      p " #{starttime}"
+    else
+      hour_from_picker_elements[starttime-24].click
+      sleep 1
+      minute_from_picker_elements[1].click
+      sleep 1
+      dismiss_picker_element.click
+      sleep 1
+      BrowserActions.js_click("//textarea[contains(@placeholder,'Optional')]")
+      p " #{starttime - 24}"
+      date_time_to_elements.first.click
+      sleep 1
+      p ">> #{current_day_elements.size}"
+      select_todays_date_from_calendar(1)
+    end
 
     ### set to time
     sleep 1
