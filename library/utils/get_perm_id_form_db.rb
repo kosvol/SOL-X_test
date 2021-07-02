5f8c4310a1ed08a8df1b6af3aa90823c9b83c6d5
gengxian <gengxian.chua@sol-x.co>
2021-07-02 21:29:32 +0800

    - increased indexdb sync

--- library/utils/get_perm_id_form_db.rb
+++ library/utils/get_perm_id_form_db.rb
@@ -2,13 +2,13 @@
 module WorkWithIndexeddb
   class << self
     def get_id_from_indexeddb(temp_id)
-      sleep 2
+      sleep 3
       $browser.execute_script("openRequest = indexedDB.open('safevue')")
-      sleep 1
+      sleep 2
       $browser.execute_script("db = openRequest.result")
-      sleep 1
+      sleep 2
       $browser.execute_script("res = db.transaction(['idMap'], 'readonly').objectStore('idMap').get('%s')"%temp_id)
-      sleep 1
+      sleep 2
       $browser.execute_script("return res.result['formId']")
     end
   end
