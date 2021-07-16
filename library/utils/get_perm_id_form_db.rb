0c4b55fc19ae1475be82c783a4937712595e9307
gengxian <gengxian.chua@sol-x.co>
2021-07-16 15:20:41 +0800

    - enhance indexedb accessor

--- library/utils/get_perm_id_form_db.rb
+++ library/utils/get_perm_id_form_db.rb
@@ -2,14 +2,31 @@
 module WorkWithIndexeddb
   class << self
     def get_id_from_indexeddb(temp_id)
-      sleep 6
-      $browser.execute_script("openRequest = indexedDB.open('safevue')")
+      open_indexdb(temp_id)
+      access_indexdb_data("formId")
+    end
+
+    private
+
+    def open_indexdb(temp_id)
       sleep 1
-      $browser.execute_script("db = openRequest.result")
-      sleep 1
-      $browser.execute_script("res = db.transaction(['idMap'], 'readonly').objectStore('idMap').get('%s')"%temp_id)
-      sleep 1
-      $browser.execute_script("return res.result['formId']")
+      begin
+        $browser.execute_script("openRequest = indexedDB.open('safevue')")
+        $browser.execute_script("db = openRequest.result")
+        $browser.execute_script("res = db.transaction(['idMap'], 'readonly').objectStore('idMap').get('%s')"%temp_id)
+      rescue
+        ">> Retrying open_indexdb"
+        open_indexdb(temp_id)
+      end
+    end
+
+    def access_indexdb_data(_data)
+      begin
+        $browser.execute_script("return res.result['#{_data}']")
+      rescue
+        p ">> Retrying access_indexdb_data"
+        access_indexdb_data(_data)
+      end
     end
   end
 end
