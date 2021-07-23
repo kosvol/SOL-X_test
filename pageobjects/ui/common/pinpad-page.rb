27d3b60e8e97f72caa0bf4280a9a7500e693761d
gx <gengxian.chua@sol-x.co>
2021-07-23 09:45:27 +0800

    - typo

--- pageobjects/ui/common/pinpad-page.rb
+++ pageobjects/ui/common/pinpad-page.rb
@@ -10,7 +10,7 @@ class PinPadPage
 
   def enter_pin(pin)
     format('%04d', pin).to_s.split('').each do |num|
-      index = num.to_i === pin_pad_elements.last ? 10 : num
+      index = num.to_i === 0 ? pin_pad_elements.last : num
       p "index >> #{index}"
       query = "//ol[@class='pin-entry']/li[%s]/button[starts-with(@class,'Button__')]"
       query = format(query, index.to_s)
