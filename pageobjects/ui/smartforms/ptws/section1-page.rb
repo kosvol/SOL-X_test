53a7bcf56cb0bb4bc0d059542d70c46ce9a03662
gx <gengxian.chua@sol-x.co>
2021-07-24 10:32:07 +0800

    - debug dynamic user capture

--- pageobjects/ui/smartforms/ptws/section1-page.rb
+++ pageobjects/ui/smartforms/ptws/section1-page.rb
@@ -27,7 +27,7 @@ class Section1Page < Section0Page
     @@section1_data_collector
   end
 
-  def set_section1_filled_data
+  def set_section1_filled_data(_entered_pin)
     rank_and_name = get_user_details_by_pin(_entered_pin)
     Log.instance.info(">> #{rank_and_name_stamp_elements.first.text}")
     @@section1_data_collector << "Created By #{rank_and_name[0]} #{rank_and_name[1]} #{rank_and_name[2]} at"
