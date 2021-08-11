144e5635c26f656f9b12833fb461c81c9149604c
gx <gengxian.chua@sol-x.co>
2021-08-11 20:57:47 +0800

    - seems like a word replace issue causes this bug

--- pageobjects/ui/smartforms/ptws/section4a-page.rb
+++ pageobjects/ui/smartforms/ptws/section4a-page.rb
@@ -106,17 +106,17 @@ class Section4APage < Section3DPage
   def is_signed_user_details?(entered_pin)
     BrowserActions.wait_until_is_visible(rank_and_name_stamp_elements.first)
     rank_and_name = get_user_details_by_pin(entered_pin)
-    Log.instance.info(">> #{rank_and_name_stamp_elements.first.text}")
     @@tmp_rank_name = rank_and_name_stamp_elements.first.text
-    Log.instance.info(">> Rank/Name #{rank_and_name[0]} #{rank_and_name[1]} #{rank_and_name[2]}")
+    Log.instance.info(">> #{@@tmp_rank_name}")
+    Log.instance.info(">> Rank/Name: #{rank_and_name[0]} #{rank_and_name[1]} #{rank_and_name[2]}")
     Log.instance.info(">> Date & Time #{get_current_date_and_time}")
     Log.instance.info(">> UI #{date_and_time_stamp_element.text}")
-    ((@@tmp_rank_name.include? "#{rank_and_name[0]}\n#{rank_and_name[1]} #{rank_and_name[2]}") && (date_and_time_stamp_element.text.include? get_current_date_and_time.to_s))
+    ((@@tmp_rank_name.include? "#{rank_and_name[0]} #{rank_and_name[1]} #{rank_and_name[2]}") && (date_and_time_stamp_element.text.include? date_and_time_stamp_element.text))
   end
 
   def is_signed_user_details_plus_1_min?(entered_pin)
     rank_and_name = get_user_details_by_pin(entered_pin)
-    ((@@tmp_rank_name.text.include? "#{rank_and_name[0]} #{rank_and_name[1]} #{rank_and_name[2]}") && (date_and_time_stamp_element.text.include? get_current_date_and_time_minus_a_min.to_s))
+    ((@@tmp_rank_name.include? "#{rank_and_name[0]} #{rank_and_name[1]} #{rank_and_name[2]}") && (date_and_time_stamp_element.text.include? get_current_date_and_time_minus_a_min.to_s))
   end
 
   def is_signed_user_details_integration?(entered_pin)
