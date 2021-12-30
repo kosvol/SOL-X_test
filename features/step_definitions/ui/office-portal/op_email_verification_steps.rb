367e408e2d7c5592025b7ae717f4436fb129e355
MDMA213 <Homersimpson213@icloud.com>
2021-12-30 12:48:49 +0300

    small fix

--- features/step_definitions/ui/office-portal/op_email_verification_steps.rb
+++ features/step_definitions/ui/office-portal/op_email_verification_steps.rb
@@ -4,7 +4,7 @@ require_relative '../../../../page_objects/office_portal/op_email_verification_p
 
 Then('EmailVerification should see all the page attributes') do
   @op_email_verification ||= OPEmailVerificationPage.new(@driver)
-  @op_email_verification.verify_verification_page_att
+  @op_email_verification.verify_verification_page_attr
 end
 
 
