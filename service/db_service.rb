7d5eecc3f36e5065c0520fdc31c7e1b69d230880
jasonchangsolx <jason.chang@sol-x.co>
2022-05-12 17:45:16 +0800

    add purge and compact for couchdb cleaning

--- service/db_service.rb
+++ service/db_service.rb
@@ -15,6 +15,10 @@ class DBService
     @couch_db_api.get_request(db_type, table)
   end
 
+  def retrieve_changes(db_type, table)
+    @couch_db_api.request_changes(db_type, table)
+  end
+
   def delete_table(db_type, table, table_response)
     payload = { docs: [] }
     vessel_name = retrieve_vessel_name
@@ -26,6 +30,22 @@ class DBService
     end
     response = @couch_db_api.request_bulk_docs(db_type, table, payload.to_json)
     @logger.info("delete form request response: #{response}")
+  end
+
+  def purge_table(db_type, table)
+    vessel_name = retrieve_vessel_name
+    changes = retrieve_changes(db_type, table)
+    changes['results'].each do |row|
+      next unless row_data_match?(table, row, vessel_name)
+      next unless row.key?('deleted') && (row['deleted'] == true)
+
+      purge_row = { "#{row['id']}": [row['changes'].first['rev']] }
+      @couch_db_api.request_purge(db_type, table, purge_row.to_json)
+    end
+  end
+
+  def compact_table(db_type, table)
+    @couch_db_api.request_compact(db_type, table)
   end
 
   private
