799acf492f8740de34c7f6b04c1547b34740faad
Konstantin Sergeenko <ksergeenko@ipolecat.com>
2022-09-21 20:17:17 +0300

    Replace the explicit waiting with the implicit one.

--- page_objects/smart_form_page.rb
+++ page_objects/smart_form_page.rb
@@ -11,6 +11,8 @@ class SmartFormsPage < BasePage
     create_entry_permit_btn: "//span[contains(.,'Entry Permit')]",
     hamburger_menu_btn: "//nav[starts-with(@class,'NavigationBar__NavBar-')]/header/button",
     ptw_state_card: "//div[starts-with(@class, 'PermitsByStatus')]//div[@role='listitem']/div[contains(., '%s')]",
+    ptw_state_counter:
+    "//div[starts-with(@class,'PermitsByStatus')]//div[@role='listitem']/div[contains(.,'%s')]/..//span[@class='stat']",
     loading_screen: "//*[starts-with(@class,'SplashScreen__LoadingIndicator')]"
   }.freeze
 
@@ -43,11 +45,10 @@ class SmartFormsPage < BasePage
 
   def click_hamburger_menu_btn
     click(SMART_FORMS[:hamburger_menu_btn])
-    sleep 1
   end
 
   def open_state_page_by_ui(page)
-    sleep 3
+    wait_for_state_counter(page)
     click(SMART_FORMS[:ptw_state_card] % page)
   end
 
@@ -61,6 +62,16 @@ class SmartFormsPage < BasePage
       sleep 0.5
       wait += 1
       raise 'time out waiting for loading screen' if wait > 30
+    end
+  end
+
+  def wait_for_state_counter(page)
+    wait = 0
+    while retrieve_text(SMART_FORMS[:ptw_state_counter] % page).to_i.zero?
+      @logger.debug "wait for state counter to be updated, retrying: #{wait} times"
+      sleep 0.5
+      wait += 1
+      raise 'time out waiting for loading' if wait > 30
     end
   end
 end
