090a1b64bf8938b053c69536be3e4ecb23bbbc7d
kosvol <99240519+kosvol@users.noreply.github.com>
2023-07-10 12:03:37 +0300

    Update db_steps.rb

--- features/step_definitions/scripts/db_steps.rb
+++ features/step_definitions/scripts/db_steps.rb
@@ -32,13 +32,3 @@ end
 And('DB service sleep {string} sec for data reloaded') do |sec|
   sleep sec.to_i
 end
-
-Given('DB service clear Manage role list') do
-  postgres = Postgres.new
-  postgres.manage_role_clear_users
-end
-
-Given('DB Manage role create {string} roles') do |option|
-  postgres = Postgres.new
-  postgres.create_roles(option)
-end
