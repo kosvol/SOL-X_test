5db61f7e77cc318ae821eff2187b1f4b80faedb6
kosvol <99240519+kosvol@users.noreply.github.com>
2023-07-24 11:42:07 +0000

    Update google_test_step.rb

--- features/step_definitions/google_test_step.rb
+++ features/step_definitions/google_test_step.rb
@@ -1,4 +1,10 @@
 Given("Google open main page") do
-    @driver.get "https://mail.google.com/"
+    @driver.get "https://google.com/gmail/about"
+    puts('test complete')
+end
+
+Given("Google find Sign in") do
+    xpath = "//a[contains(.,'Sign in')]"
+    @driver.find_element(xpath)"
     puts('test complete')
 end
